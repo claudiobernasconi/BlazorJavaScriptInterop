9e01126c4e69ac43fc79e39e16ff7a83776959e9
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-07-09 15:25:07 +0200

    Initial commit

---
+++ BlazorJavaScriptInterop/wwwroot/script.js
@@ -0,0 +1,9 @@
+﻿function showMessage(message) {
+    const result = 2 * 21
+    alert(message + " " + result)
+}
+
+function calculateAge(yearOfBirth) {
+    const currentYear = new Date().getFullYear()
+    return currentYear - yearOfBirth
+}
