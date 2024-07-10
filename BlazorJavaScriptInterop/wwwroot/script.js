046118b4de6e8f7ab505f7f63aa2db6a9d4b4631
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-07-10 14:27:00 +0200

    Add chartJs and implement a simple bar chart

--- BlazorJavaScriptInterop/wwwroot/script.js
+++ BlazorJavaScriptInterop/wwwroot/script.js
@@ -7,3 +7,35 @@ function calculateAge(yearOfBirth) {
     const currentYear = new Date().getFullYear()
     return currentYear - yearOfBirth
 }
+
+function createChart(htmlElementId, data, label) {
+    new Chart(
+        document.getElementById(htmlElementId),
+        {
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.year),
+                datasets: [
+                    {
+                        label: label,
+                        data: data.map(row => row.salary)
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+        }
+    );
+}
+
+function disposeChart(htmlElementId) {
+    const chartStatus = Chart.getChart(htmlElementId);
+    if (chartStatus != undefined) {
+        chartStatus.destroy();
+    }
+}
