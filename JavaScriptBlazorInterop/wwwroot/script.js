e3e415b1d10de9e7e415ac4cf71542a8630ad47e
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-07-29 16:52:20 +0200

    Add JavaScript to .NET interoperability example

---
+++ JavaScriptBlazorInterop/wwwroot/script.js
@@ -0,0 +1,13 @@
+﻿function generateChatMessages(dotnetObjectReference) {
+    dotnetObjectReference.invokeMethodAsync("AddMessage", "Hello from JavaScript!")
+    setTimeout(() => {
+        dotnetObjectReference.invokeMethodAsync("AddMessage", "Are you still there?!")
+    }, 2000);
+    setTimeout(async () => {
+        const name = await dotnetObjectReference.invokeMethodAsync("GetName")
+        dotnetObjectReference.invokeMethodAsync("AddMessage", "Your name is: " + name + ".")
+    }, 3000);
+    setTimeout(() => {
+        dotnetObjectReference.invokeMethodAsync("AddMessage", "Blazor and JavaScript live side-by-side!")
+    }, 5000);
+}
